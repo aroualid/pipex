603ed4f4702cdd19f09451af17c0d8521980ebd9
aroualid <aroualid@student.42.fr>
2024-03-21 19:11:31 +0100

    ok

--- exec.c
+++ exec.c
@@ -6,13 +6,13 @@
 /*   By: aroualid <aroualid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 11:54:50 by aroualid          #+#    #+#             */
-/*   Updated: 2024/03/21 14:14:42 by aroualid         ###   ########.fr       */
+/*   Updated: 2024/03/21 19:07:22 by aroualid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char *find_path(char **env, char *av)
+char	*find_path(char **env, char *av)
 {
 	char **folders = NULL;
 	char *path = NULL;
@@ -23,18 +23,35 @@ char *find_path(char **env, char *av)
 
 	i = 0;
 	j = 0;
-	while (ft_strlen(env[i]) <= 5 || ft_strncmp(env[i], "PATH", 5) != 0)
+	while (ft_strlen(env[i]) <= 4 || ft_strncmp(env[i], "PATH", 4) != 0)
 		i++;
 	folders = ft_split(env[i], ':');
 	endfile = ft_strjoin("/", av);
 	while (folders[j])
 	{
 		path = ft_strjoin(folders[j], endfile);
-		if (access(path, F_OK | X_OK))
+		if (access(path, F_OK | X_OK) == 0)
 			return (path);
 		else
-			i++;
+			j++;
 	}
 	return (write (1, "command not found : ", 21), 
 		write(1, av, ft_strlen(av)), NULL);
+}
+
+
+char	**find_cmd(char *av)
+{
+	char **cmd;
+
+	cmd = NULL;
+	cmd = ft_split(av, ' ');
+	if (cmd)
+		return cmd;
+	return (NULL);	
+}
+
+int	apply_exec(char *path, char **cmd, char **env)
+{
+	return(execve(path, cmd, env));
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: aroualid <aroualid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 17:17:52 by aroualid          #+#    #+#             */
-/*   Updated: 2024/03/21 14:09:12 by aroualid         ###   ########.fr       */
+/*   Updated: 2024/03/21 19:08:13 by aroualid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <limits.h>
 # include <stdint.h>
 # include <unistd.h>
+# include <stdio.h>
 
 char	**ft_split(char const *s, char c);
 void	*ft_calloc(size_t nmemb, size_t size);
@@ -25,5 +26,7 @@ void	ft_bzero(void *s, size_t n);
 int		ft_strncmp( const char *first, const char *second, size_t length);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
-
+char	*find_path(char **env, char *av);
+char	**find_cmd(char *av);
+int	apply_exec(char *path, char **cmd, char **env);
 #endif
