45f6f4174dba89328779c1ece3cae064171a3aeb
aroualid <aroualid@student.42.fr>
2024-03-22 13:58:30 +0100

    ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aroualid <aroualid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 17:18:21 by aroualid          #+#    #+#             */
-/*   Updated: 2024/03/21 19:10:32 by aroualid         ###   ########.fr       */
+/*   Updated: 2024/03/22 13:52:56 by aroualid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 int main(int ac, char **av, char **env)
 {
+	int id;
+	int id2;
+
+	id = 0;
+	id2 = 0;
 	if (ac != 5)
 		return (0);
 	else
@@ -22,7 +27,15 @@ int main(int ac, char **av, char **env)
 		char *path3 = find_path(env, av[3]);
 		char **cmd2 = find_cmd(av[2]);
 		char **cmd3 = find_cmd(av[3]);
-		apply_exec(path2, cmd2, env);
-		apply_exec(path3, cmd3, env);
+		id = fork ();
+		if (id == 0)
+		{
+			apply_exec(path2, cmd2, env);
+		}
+		id2 = fork();
+		if (id2 == 0)
+		{
+			apply_exec(path3, cmd3, env);
+		}
 	}
 }
